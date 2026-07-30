73d7e0962978b8eb582b73ab070fae0f31f40db6
abdur-rehman-ali <abdurrehmanali369@gmail.com>
2026-07-31 04:29:14 +0500

    Add Django REST framework to project dependencies and settings

--- config/settings.py
+++ config/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # Third party
     "django_extensions",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -112,3 +113,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
